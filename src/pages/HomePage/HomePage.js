c6eff576f8471fb49989971878e5f04a4db785a6
clubd <danenrmar@gmail.com>
2024-02-15 17:04:08 -0500

    create homepage for the search box component

---
+++ src/pages/HomePage/HomePage.js
@@ -0,0 +1,11 @@
+import SearchBar from "../../components/SearchBar/SearchBar";
+
+function HomePage() {
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    )
+}
+
+export default HomePage;
