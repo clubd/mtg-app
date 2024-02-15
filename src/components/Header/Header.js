c0af1aff65081bbae2cb2d2576489c19e86c3776
clubd <danenrmar@gmail.com>
2024-02-15 09:42:15 -0500

    change header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,16 @@
 import Navbar from "../Navbar/Navbar";
+import { Link } from "react-router-dom";
 
 function Header() {
     return (
-        <Navbar />
+        <header className="header">
+            <h1 className="header_heading">On The Stack</h1>
+            <nav className="header_nav">
+                <ul className="header_list">
+                    <li className="header_list-item"><Link to="/">Home</Link></li>
+                </ul>
+            </nav>
+        </header>
     );
 }
 
