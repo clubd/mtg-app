f46f414e92016ec3ee0eaad4a1a830c91909ec70
clubd <danenrmar@gmail.com>
2024-02-15 18:29:09 -0500

    added function to manage stack

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,9 +1,17 @@
+import { useState } from "react";
 import SearchBar from "../../components/SearchBar/SearchBar";
+import StackBox from "../../components/StackBox/StackBox";
 
 function HomePage() {
+    const [stack, setStack] = useState([]);
+
+    const handleAddToStack = (card) => {
+        setStack(prevStack => [...prevStack, card]);
+    };
     return (
         <div>
-            <SearchBar />
+            <SearchBar onAddToStack={handleAddToStack} />
+            <StackBox stack={stack} />
         </div>
     )
 }
